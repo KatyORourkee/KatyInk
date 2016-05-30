54af32b2a82d5f2d38d6ca4f077423bb05234354
katy <katysorourke@gmail.com>
2016-05-30 10:18:48 -0700

    Committing first version of Katy Ink.

--- KatyInk/App_Start/BundleConfig.cs
+++ KatyInk/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace KatyInk
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                      "~/Scripts/knockout-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
--- KatyInk/Controllers/HomeController.cs
+++ KatyInk/Controllers/HomeController.cs
@@ -13,17 +13,13 @@ namespace KatyInk.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Calculator()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult YodaInstructor()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
